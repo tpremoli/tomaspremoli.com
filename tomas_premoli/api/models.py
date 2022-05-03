b4f5fab8092d16424f606db545ffd9863ecaed8e
tpremoli <tomas@premoli.org>
2022-05-03 17:52:34 +0100

    Rerouted media to /api/media

--- tomas_premoli/api/models.py
+++ tomas_premoli/api/models.py
@@ -1,29 +1,38 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
-from .storage import OverwriteStorage
 import os
 
 # Create your models here.
 class MyData(models.Model):
     def rename_pic(instance, filename):
         ext = filename.split('.')[-1]
-        return '{}.{}'.format("api/media/me/pic", ext)
+        return '{}.{}'.format("me/pic", ext)
         
     def rename_pdf(instance, filename):
         ext = filename.split('.')[-1]
-        return '{}.{}'.format("api/media/me/cv", ext)
+        return '{}.{}'.format("me/cv", ext)
 
-    pic = models.ImageField(upload_to=rename_pic, storage=OverwriteStorage(), max_length=None)
+    pic = models.ImageField(upload_to=rename_pic)
     fname = models.CharField(default="Tomas", max_length=255)
     lname = models.CharField(default="Premoli", max_length=255)
     aboutme = models.TextField(default="")
 
     cv = models.FileField(upload_to=rename_pdf,
-                    validators=[FileExtensionValidator(allowed_extensions=['pdf'])],
-                    storage=OverwriteStorage(), max_length=None)
+                    validators=[FileExtensionValidator(allowed_extensions=['pdf'])])
 
     github_link = models.CharField(default="https://github.com/tpremoli", max_length=255)
     linkedin_link = models.CharField(default="https://www.linkedin.com/in/tomas-premoli-008016144/", max_length=255)
+    
+    def save(self, *args, **kwargs):
+            try:
+                this = MyData.objects.get(id=self.id)
+                if this.pic != self.pic:
+                    this.pic.delete()
+                if this.cv != self.cv:
+                    this.cv.delete()
+            except: pass
+            super(MyData, self).save(*args, **kwargs)
+
 
 class PortfolioEntry(models.Model):
     thumbnailpic = models.ImageField()
--- tomas_premoli/tomas_premoli/urls.py
+++ tomas_premoli/tomas_premoli/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
 ]
+
+# Need to change this before final deployment as django recommends to do this another way
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
