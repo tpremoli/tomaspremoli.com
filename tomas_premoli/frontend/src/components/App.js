56cca25c1daa09c286567c9140459f4912497c89
tpremoli <tomas@premoli.org>
2022-05-04 15:03:44 +0100

    Began implementing basic template

--- tomas_premoli/frontend/src/components/App.js
+++ tomas_premoli/frontend/src/components/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import Home from "./Home";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    Redirect,
+} from "react-router-dom";
 
-export default class App extends Component{
+export default class App extends Component {
     constructor(props) {
         super(props);
     }
 
-    render(){
-        return <h1>Barebones app.js</h1>;
+
+    render() {
+        return (
+            <Router>
+                <ThemeProvider theme={theme}>
+                    <Routes>
+                        <Route exact path="/" element={<Home />} />
+                        {/* <Route path="/about-me" element={<x />} /> */}
+                        {/* <Route path="/portfolio" element={<Portfolio />} /> */}
+                        {/* <Route path="/resume" element={<Resume />} /> */}
+                        {/* <Route path="/contact-me" element={<ContactMe />} /> */}
+                    </Routes>
+                </ThemeProvider>
+            </Router >
+        );
     }
 }
 
+const theme = createTheme();
 const appDiv = document.getElementById("app");
 render(<App />, appDiv);
