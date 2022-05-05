1b3de238a42b2b7b6348418fc48c1bcba97eb774
tpremoli <tomas@premoli.org>
2022-05-05 19:04:40 +0100

    Removed unnecessary Home code

--- tomas_premoli/frontend/src/components/Home.js
+++ tomas_premoli/frontend/src/components/Home.js
@@ -19,7 +19,9 @@ export default function Home() {
     return (
         <Container disableGutters maxWidth={false}>
             <CssBaseline />
+            {/* Header */}
             <SelectionMenu />
+            {/* End Header */}
             <main>
                 {/* Hero unit */}
                 <Box
@@ -56,41 +58,7 @@ export default function Home() {
                         </Stack>
                     </Container>
                 </Box>
-                <Container sx={{ py: 8 }} maxWidth="md">
-                    {/* End hero unit */}
-                    <Grid container spacing={4}>
-                        {cards.map((card) => (
-                            <Grid item key={card} xs={12} sm={6} md={4}>
-                                <Card
-                                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                                >
-                                    <CardMedia
-                                        component="img"
-                                        sx={{
-                                            // 16:9
-                                            pt: '56.25%',
-                                        }}
-                                        image="https://source.unsplash.com/random"
-                                        alt="random"
-                                    />
-                                    <CardContent sx={{ flexGrow: 1 }}>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            Heading
-                                        </Typography>
-                                        <Typography>
-                                            This is a media card. You can use this section to describe the
-                                            content.
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button size="small">View</Button>
-                                        <Button size="small">Edit</Button>
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Container>
+                
             </main>
             {/* Footer */}
             <Footer />
