8437e417e4cf9e39e8a010f17d5d6c63405ec15f
tpremoli <tomas@premoli.org>
2022-05-14 00:47:56 +0100

    Applied template to aboutme

---
+++ tomas_premoli/frontend/src/components/AboutMe.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import CircularProgress from '@mui/material/CircularProgress';
+import Footer from './Footer';
+import SelectionMenu from './SelectionMenu';
+
+
+export default function AboutMe() {
+    const [myData, setMyData] = useState({});
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getAllNodes();
+    }, []);
+
+    const getAllNodes = () => {
+        fetch("./api/my-data?format=json")
+            .then(response => response.json())
+            .then((data) => {
+                setMyData(data);
+                setLoading(false);
+            });
+    };
+
+    if (isLoading) {
+        return (
+            <Box sx={{
+                display: 'flex', alignItems: "center",
+                justifyContent: "center", height: "100%", width: "100%",
+            }}>
+                <CircularProgress sx={{ display: 'flex', alignSelf: "center" }} />
+            </Box >
+        );
+    }
+
+    return (
+        <Container disableGutters maxWidth={false}>
+            <CssBaseline />
+            {/* Header */}
+            <SelectionMenu />
+            {/* End Header */}
+            <main>
+                {/* Hero unit */}
+                <Box
+                    sx={{
+                        bgcolor: 'background.paper',
+                        pt: 8,
+                        pb: 6,
+                    }}
+                >
+                    <Container maxWidth="sm">
+                    </Container>
+                </Box>
+
+            </main>
+            {/* Footer */}
+            <Footer />
+            {/* End footer */}
+        </Container >
+    );
+}
