052fb11361beb9349ecc73cca71fbd38551bd196
tpremoli <tomas@premoli.org>
2022-05-19 00:38:59 +0100

    Added icons and titles to cards

--- tomas_premoli/frontend/src/components/Experiences.js
+++ tomas_premoli/frontend/src/components/Experiences.js
@@ -9,6 +9,9 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import CardMedia from '@mui/material/CardMedia';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import Container from '@mui/material/Container';
 import CircularProgress from '@mui/material/CircularProgress';
 import Footer from './Footer';
@@ -16,35 +19,6 @@ import SelectionMenu from './SelectionMenu';
 
 
 export default function Experiences() {
-    const [myData, setMyData] = useState({});
-    const [isLoading, setLoading] = useState(true);
-
-    useEffect(() => {
-        getAllNodes();
-    }, []);
-
-    const getAllNodes = () => {
-        fetch("./api/my-data?format=json")
-            .then(response => response.json())
-            .then((data) => {
-                setMyData(data);
-                setLoading(false);
-            });
-    };
-
-    if (isLoading) {
-        return (
-            <Box sx={{
-                display: 'flex', alignItems: "center",
-                justifyContent: "center", height: "100%", width: "100%",
-            }}>
-                <CircularProgress sx={{ display: 'flex', alignSelf: "center" }} />
-            </Box >
-        );
-    }
-
-    console.log(myData);
-
     return (
         <Grid container spacing={4}
             justifyContent="center"
@@ -53,11 +27,28 @@ export default function Experiences() {
             <Grid item sx={{
                 display: 'flex',
                 width: "90%",
-                flexDirection: { xs: 'column', sm: 'row' }
+                flexDirection: { xs: "column", sm: 'column', md: 'row' }
             }}>
                 <Card
                     sx={{ height: '100%', p: "2%", m: "7px" }}
                 >
+                    <CardContent sx={{ flexGrow: 1, flexWrap: "nowrap" }}>
+                        <Typography
+                            sx={{ fontFamily: "Consolas" }}
+                            gutterBottom variant="h4" component="h2">
+                            Experiences
+                            <WorkOutlineIcon
+                                sx={{
+                                    width: "36px",
+                                    height: "36px",
+                                    color: "black",
+                                    verticalAlign: "middle",
+                                    m: "3%"
+                                }}
+                            />
+                        </Typography>
+                    </CardContent>
+
                     <CardMedia
                         component="img"
                         image="https://source.unsplash.com/random"
@@ -80,6 +71,23 @@ export default function Experiences() {
                 <Card
                     sx={{ height: '100%', p: "2%", m: "7px" }}
                 >
+                    <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography
+                            sx={{ fontFamily: "Consolas" }}
+                            gutterBottom variant="h4" component="h2">
+                            Education
+                            <SchoolOutlinedIcon
+                                sx={{
+                                    width: "36px",
+                                    height: "36px",
+                                    color: "black",
+                                    verticalAlign: "middle",
+                                    m: "3%"
+                                }}
+                            />
+                        </Typography>
+                    </CardContent>
+
                     <CardMedia
                         component="img"
                         image="https://source.unsplash.com/random"
@@ -102,6 +110,23 @@ export default function Experiences() {
                 <Card
                     sx={{ height: '100%', p: "2%", m: "7px" }}
                 >
+                    <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography
+                            sx={{ fontFamily: "Consolas" }}
+                            gutterBottom variant="h4" component="h2">
+                            Skills
+                            <SettingsOutlinedIcon
+                                sx={{
+                                    width: "36px",
+                                    height: "36px",
+                                    color: "black",
+                                    verticalAlign: "middle",
+                                    m: "3%"
+                                }}
+                            />
+                        </Typography>
+                    </CardContent>
+
                     <CardMedia
                         component="img"
                         image="https://source.unsplash.com/random"
@@ -122,6 +147,6 @@ export default function Experiences() {
                     </CardActions>
                 </Card>
             </Grid>
-        </Grid>
+        </Grid >
     );
 }
