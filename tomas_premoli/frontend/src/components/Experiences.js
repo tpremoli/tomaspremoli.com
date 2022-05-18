be7e618531cfc3e7ac5ce5cd25ef0af7deb2e8fa
tpremoli <tomas@premoli.org>
2022-05-19 00:09:33 +0100

    Added appropriate experience styling

--- tomas_premoli/frontend/src/components/Experiences.js
+++ tomas_premoli/frontend/src/components/Experiences.js
@@ -46,93 +46,82 @@ export default function Experiences() {
     console.log(myData);
 
     return (
-        <Container sx={{ py: 8 }} maxWidth="md">
-            <Grid container spacing={4}>
-                <Grid item xs={12} sm={6} md={4}>
-                    <Card
-                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                    >
-                        <CardMedia
-                            component="img"
-                            sx={{
-                                // 16:9
-                                pt: '10%',
-                            }}
-                            image="https://source.unsplash.com/random"
-                            alt="random"
-                        />
-                        <CardContent sx={{ flexGrow: 1 }}>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Heading
-                            </Typography>
-                            <Typography>
-                                This is a media card. You can use this section to describe the
-                                content.
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small">View</Button>
-                            <Button size="small">Edit</Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <Card
-                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                    >
-                        <CardMedia
-                            component="img"
-                            sx={{
-                                // 16:9
-                                pt: '10%',
-                            }}
-                            image="https://source.unsplash.com/random"
-                            alt="random"
-                        />
-                        <CardContent sx={{ flexGrow: 1 }}>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Heading
-                            </Typography>
-                            <Typography>
-                                This is a media card. You can use this section to describe the
-                                content.
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small">View</Button>
-                            <Button size="small">Edit</Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <Card
-                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                    >
-                        <CardMedia
-                            component="img"
-                            sx={{
-                                // 16:9
-                                pt: '10%',
-                            }}
-                            image="https://source.unsplash.com/random"
-                            alt="random"
-                        />
-                        <CardContent sx={{ flexGrow: 1 }}>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Heading
-                            </Typography>
-                            <Typography>
-                                This is a media card. You can use this section to describe the
-                                content.
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small">View</Button>
-                            <Button size="small">Edit</Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
+        <Grid container spacing={4}
+            justifyContent="center"
+            alignItems="center"
+        >
+            <Grid item sx={{
+                display: 'flex',
+                width: "90%",
+                flexDirection: { xs: 'column', sm: 'row' }
+            }}>
+                <Card
+                    sx={{ height: '100%', p: "2%", m: "7px" }}
+                >
+                    <CardMedia
+                        component="img"
+                        image="https://source.unsplash.com/random"
+                        alt="random"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Heading
+                        </Typography>
+                        <Typography>
+                            This is a media card. You can use this section to describe the
+                            content.
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">View</Button>
+                        <Button size="small">Edit</Button>
+                    </CardActions>
+                </Card>
+                <Card
+                    sx={{ height: '100%', p: "2%", m: "7px" }}
+                >
+                    <CardMedia
+                        component="img"
+                        image="https://source.unsplash.com/random"
+                        alt="random"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Heading
+                        </Typography>
+                        <Typography>
+                            This is a media card. You can use this section to describe the
+                            content.
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">View</Button>
+                        <Button size="small">Edit</Button>
+                    </CardActions>
+                </Card>
+                <Card
+                    sx={{ height: '100%', p: "2%", m: "7px" }}
+                >
+                    <CardMedia
+                        component="img"
+                        image="https://source.unsplash.com/random"
+                        alt="random"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Heading
+                        </Typography>
+                        <Typography>
+                            This is a media card. You can use this section to describe the
+                            content.
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">View</Button>
+                        <Button size="small">Edit</Button>
+                    </CardActions>
+                </Card>
             </Grid>
-        </Container>
+        </Grid>
     );
 }
