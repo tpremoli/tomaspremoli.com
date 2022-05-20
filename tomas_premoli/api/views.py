7b88030a88b8f352932494e2fc9d3b0c40ea3559
tpremoli <tomas@premoli.org>
2022-05-20 22:16:01 +0100

    Improved EES view formatting

--- tomas_premoli/api/views.py
+++ tomas_premoli/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework import generics
 from rest_framework.views import APIView
 from collections import OrderedDict
+import datetime
 
 from .models import ContactEntry, MyData, Experience, Education, Skills
 from .serializers import MyDataSerializer, ContactEntrySerializer, ExperienceSerializer, EducationSerializer, SkillsSerializer
@@ -26,8 +27,57 @@ class GetEES(APIView):
         exps = Experience.objects.order_by("-start_date")
         exps_data = ExperienceSerializer(exps, many=True).data
 
+        for exp in exps_data:
+            if(exp["end_date"] == None):
+                exp["end_date"] = "Present"
+
+                curr_date = datetime.datetime.now()
+                start_date = datetime.datetime.strptime(exp["start_date"], "%Y-%m-%d")
+
+                duration = abs(curr_date.year - start_date.year) * 12 + abs(curr_date.month - start_date.month)
+
+                if duration == 0 or duration == 1:
+                    exp["duration"] = "1 mo"
+                else:
+                    exp["duration"] = str(duration) + " mos"
+            else:
+                start_date = datetime.datetime.strptime(exp["start_date"], "%Y-%m-%d")
+                end_date = datetime.datetime.strptime(exp["end_date"], "%Y-%m-%d")
+
+                duration = abs(end_date.year - start_date.year) * 12 + abs(end_date.month - start_date.month)
+
+                if duration == 0 or duration == 1:
+                    exp["duration"] = "1 mo"
+                else:
+                    exp["duration"] = str(duration) + " mos"
+
         edcs = Education.objects.order_by("-start_date")
         edcs_data = EducationSerializer(edcs, many=True).data
+
+        for edc in edcs_data:
+            if(edc["end_date"] == None):
+                edc["end_date"] = "Present"
+
+                curr_date = datetime.datetime.now()
+                start_date = datetime.datetime.strptime(edc["start_date"], "%Y-%m-%d")
+
+                duration = abs(curr_date.year - start_date.year) * 12 + abs(curr_date.month - start_date.month)
+
+                if duration == 0 or duration == 1:
+                    edc["duration"] = "1 mo"
+                else:
+                    edc["duration"] = str(duration) + " mos"
+            else:
+                start_date = datetime.datetime.strptime(edc["start_date"], "%Y-%m-%d")
+                end_date = datetime.datetime.strptime(edc["end_date"], "%Y-%m-%d")
+
+                duration = abs(end_date.year - start_date.year) * 12 + abs(end_date.month - start_date.month)
+
+                if duration == 0 or duration == 1:
+                    edc["duration"] = "1 mo"
+                else:
+                    edc["duration"] = str(duration) + " mos"
+
 
         skills = Skills.objects.order_by()
         skills_data = SkillsSerializer(skills, many=True).data
@@ -38,7 +88,10 @@ class GetEES(APIView):
             'skills': skills_data
             })
 
+        print(data)
+
         return Response(data, status=status.HTTP_200_OK)
+
 
 
 class ContactMe(APIView):
