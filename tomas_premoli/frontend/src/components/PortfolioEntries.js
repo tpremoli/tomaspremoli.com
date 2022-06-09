ad793145d574e3243030dde7962b0473e19cbde3
tpremoli <tomas@premoli.org>
2022-06-10 00:32:03 +0100

    Implemented loading portfolio cards

--- tomas_premoli/frontend/src/components/PortfolioEntries.js
+++ tomas_premoli/frontend/src/components/PortfolioEntries.js
@@ -38,36 +38,56 @@ export default function PortfolioEntries() {
 
     if (isLoading) {
         return (
-            <Grid container spacing={4}
-                justifyContent="center"
-                alignItems="center"
-            >
-                <Grid item sx={{
-                    display: 'flex',
-                    width: "90%",
-                    flexDirection: { xs: "column", sm: 'column', md: 'row' }
-                }}>
-
+            <Container sx={{ py: 8 }} maxWidth="md">
+                <Grid container spacing={4}>
+                    {[1, 2, 3, 4, 5, 6].map((entry) => (
+                        <Grid item key={entry} xs={12} sm={6} md={4} width={300} height={200}>
+                            <Skeleton
+                                sx={{ height: '100%', width: '100%' }}
+                            />
+                        </Grid>
+                    ))}
                 </Grid>
-            </Grid >
+            </Container >
         );
     }
 
     console.log(portfolioEntries);
 
     return (
-        <Grid container spacing={4}
-            justifyContent="center"
-            alignItems="center"
-        >
-            <Grid item sx={{
-                display: 'flex',
-                width: "90%",
-                flexDirection: { xs: "column", sm: 'column', md: 'row' }
-            }}>
-                <Skeleton />
-                <Skeleton />
+        <Container sx={{ py: 8 }} maxWidth="md">
+            <Grid container spacing={4}>
+                {[1, 2, 3, 4, 5, 6].map((entry) => (
+                    <Grid item key={entry} xs={12} sm={6} md={4}>
+                        <Card
+                            sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                        >
+                            <CardMedia
+                                component="img"
+                                sx={{
+                                    // 16:9
+                                    pt: '56.25%',
+                                }}
+                                image="https://source.unsplash.com/random"
+                                alt="random"
+                            />
+                            <CardContent sx={{ flexGrow: 1 }}>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    Heading
+                                </Typography>
+                                <Typography>
+                                    This is a media card. You can use this section to describe the
+                                    content.
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">View</Button>
+                                <Button size="small">Edit</Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
             </Grid>
-        </Grid >
+        </Container >
     );
 }
