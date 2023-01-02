af59c4057f259b3796d46d712eb24ca794fa5ed1
tpremoli <tomas@premoli.org>
2023-01-02 21:43:09 +0000

    added new ssl options for django

--- tomas_premoli/tomas_premoli/settings.py
+++ tomas_premoli/tomas_premoli/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+
+SECURE_SSL_REDIRECT = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -171,7 +172,7 @@ if 'AWS_ACCESS_KEY_ID' in os.environ:
     EMAIL_BACKEND = 'django_ses.SESBackend'
     SERVER_EMAIL = 'tpremoli-notif@tomaspremoli.com'
 else:
-    MEDIA_ROOT = os.path.join(BASE_DIR, '/api/media')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'api/media')
     MEDIA_URL = '/'
     USING_AWS = False
 
