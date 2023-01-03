8ed9e4b420472b8813560fad3688957a5acfcd2b
tpremoli <tomas@premoli.org>
2023-01-03 04:53:16 +0000

    Added buttonstacks to tutoring page

--- tomas_premoli/frontend/src/components/Tutoring.js
+++ tomas_premoli/frontend/src/components/Tutoring.js
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
+import ReactMarkdown from 'react-markdown'
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
 import Container from '@mui/material/Container';
 import Footer from './Footer';
 import SelectionMenu from './SelectionMenu';
-import ReactMarkdown from 'react-markdown'
+import ButtonStacks from './ButtonStacks';
 
 
 export default function Tutoring() {
@@ -96,28 +93,15 @@ export default function Tutoring() {
                         </Container >
                     }
 
-                    <Container maxWidth="sm">
-                        <Typography variant="h5" align="center" paragraph>
+                    <Container sx={{ width: "100%" }}>
+                        <Typography marginX="auto" maxWidth="sm" variant="h5" align="center" paragraph>
                             In essence, I have experience and knowledge in many fields and I'm always happy to help!
                         </Typography>
-                        <Typography variant="h5" align="center" paragraph>
+                        <Typography marginX="auto" maxWidth="sm" variant="h5" align="center" paragraph>
                             If you'd like to consult with me, feel free to reach out below, or at my email <a href="mailto:tomas@premoli.org">tomas@premoli.org</a>
                         </Typography>
-                        <Stack
-                            sx={{ pt: 2 }}
-                            direction="row"
-                            spacing={2}
-                            justifyContent="center"
-                        >
-                            <Button to="/resume" component={Link} variant="contained">My Resume</Button>
-                            <Button to="/contact-me" component={Link} variant="contained">Contact Me</Button>
-                            <Button color="linkedin" variant="contained" align="center" href={links.linkedin_link} startIcon={<LinkedInIcon />}>
-                                LinkedIn
-                            </Button>
-                            <Button color="github" variant="contained" align="center" href={links.github_link} startIcon={<GitHubIcon />}>
-                                GitHub
-                            </Button>
-                        </Stack>
+
+                        <ButtonStacks />
 
                         <Typography spacing={2} sx={{ pt: 8 }} variant="h5" align="center" color="text.secondary" paragraph>
                             Not convinced? Check out my <Link to="/portfolio">Portfolio</Link>
