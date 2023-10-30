474c6ebee2e4a69fd2d6ec2d4923e31dfd3a5d15
tpremoli <tomas@premoli.org>
2023-10-30 23:21:52 +0000

    fixed https config

--- tomas_premoli/.platform/hooks/postdeploy/00_create_log_files.sh
+++ tomas_premoli/.platform/hooks/postdeploy/00_create_log_files.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-if test -f "$DJANGO_LOG_FILE_PATH";
-then
-  echo "$DJANGO_LOG_FILE_PATH exists"
+
+DJANGO_LOG_FILE_PATH="/var/log/django.log"
+DJANGO_LOG_FILE_DIR=$(dirname "$DJANGO_LOG_FILE_PATH")
+
+# Check if the directory exists
+if [ ! -d "$DJANGO_LOG_FILE_DIR" ]; then
+    mkdir -p "$DJANGO_LOG_FILE_DIR"
+    chown webapp:webapp "$DJANGO_LOG_FILE_DIR"
+fi
+
+# Check if the file exists
+if [ -f "$DJANGO_LOG_FILE_PATH" ]; then
+    echo "$DJANGO_LOG_FILE_PATH exists"
 else
-  # create log file
-  touch $DJANGO_LOG_FILE_PATH
+    # create log file
+    touch "$DJANGO_LOG_FILE_PATH"
 fi
 
 # set log file owner (we are currently "root", but the app runs as "webapp")
-chown webapp:webapp $DJANGO_LOG_FILE_PATH
+chown webapp:webapp "$DJANGO_LOG_FILE_PATH"
+sudo chmod 755 "$DJANGO_LOG_FILE_DIR"
